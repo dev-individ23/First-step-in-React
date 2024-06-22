d119dbd4ac3e6ae5b60d89fa66ded625c26fa4bc
dev-individ23 <jsdeveloper2332@gmail.com>
2024-06-22 20:36:49 +0300

    add counter button

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
-import { nanoid } from 'nanoid';
 import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-	const [user,setUser] = useState({
-		name:'John',
-		surname: 'Bones',
-		age:30
-	})
-
-	const [isBan,setIsBan] = useState(true);
+	const  [count,setCount] = useState(1)
 
 	return <div>
-		<p>{isBan ? user.name : ''}</p>
-		<p>{isBan ? user.surname : ''}</p>
-		<p>{isBan ? user.age : ''}</p>
-
-		<button onClick={()=>{setUser({name: 'Alex',surname: 'Liskovich',age:32})}}>ChangeUser</button>
-		
-		{isBan && <button onClick={() => {setIsBan(false)}}>DeactivateUser</button>}
-            
-    {!isBan && <button onClick={() => {setIsBan(true)}}>ActivateUser</button>}
+		<button onClick={()=>{setCount(count+1)}}>+</button>
+		<p>{count}</p>
+		<button onClick={()=>{setCount(count-1)}}>-</button>
 	</div>
 
 }
